fab9428226ce48aaf3a0375a68b8acc2f417346f
unique username <43068204+beanfacts@users.noreply.github.com>
2018-10-06 21:42:15 +0700

    Add files via upload
    Increased accuracy and removed locked_tracks issue

--- cozmo_finder.py
+++ cozmo_finder.py
@@ -22,9 +22,11 @@ GNU General Public License for more details.
 try:
 	import asyncio, functools, math, sys, cozmo, cv2, time, os
 	import numpy as np
+	import copy
 	from matplotlib import pyplot
 	from cozmo.util import degrees, distance_mm, radians, speed_mmps, Vector2
 	from cozmo.lights import Color, Light
+	from cozmo.objects import CustomObject, CustomObjectMarkers, CustomObjectTypes
 	from PIL import Image, ImageColor, ImageDraw, ImageStat
 except ImportError:
 	print("You are missing required packages!")
@@ -32,11 +34,17 @@ except ImportError:
 	input()
 	sys.exit()
 
+def handle_object_appeared(evt, **kw):
+	if isinstance(evt.obj, CustomObject):
+		return evt.obj.object_type
+
 def fip(robot, af):
+
 	ok = False
 	for i in range(10):
+		
 		try:
-			FoundCube = robot.world.wait_for_observed_light_cube(timeout=1)
+			FoundCube = robot.world.wait_for_observed_light_cube(timeout=0.5)
 			if FoundCube not in af:
 				ok = True
 				break
@@ -44,48 +52,68 @@ def fip(robot, af):
 				FoundCube = None
 		except asyncio.TimeoutError:
 			pass
+		
 		robot.turn_in_place(degrees(36)).wait_for_completed()
 	return FoundCube, ok
 
 def cube_return(robot):	
 	
-	robot.camera.color_image_enabled = True
+	# This object will be the drop-off point for the cubes.
+	robot.add_event_handler(cozmo.objects.EvtObjectAppeared, handle_object_appeared)
+	dropoff = robot.world.define_custom_cube(CustomObjectTypes.CustomType00, CustomObjectMarkers.Hexagons2, 44, 30, 30, True)
+	
+	# robot.camera.color_image_enabled = True
 
 	stordist = 200
 	storstep = 50
 	num_cubes = 3
 
 	if robot.is_on_charger == False:
-		robot.say_text("Place on charger!", voice_pitch=-1, duration_scalar=0.5).wait_for_completed()
+		robot.say_text("Off charger.", voice_pitch=-1, duration_scalar=0.5).wait_for_completed()
 		while robot.is_on_charger == False:
 			time.sleep(1)
 
 	charger = robot.world.charger
 	
 	robot.drive_off_charger_contacts().wait_for_completed()
-	robot.drive_straight(distance_mm(120), speed_mmps(100)).wait_for_completed()
-	cozmo.world.World.request_nav_memory_map(robot, 1)
+	robot.drive_straight(distance_mm(150), speed_mmps(100)).wait_for_completed()
+	robot.turn_in_place(degrees(-90)).wait_for_completed()
+	robot.drive_straight(distance_mm(150), speed_mmps(100)).wait_for_completed()
+	robot.turn_in_place(degrees(-90)).wait_for_completed()
+	drop = copy.deepcopy(robot.pose)
+	robot.turn_in_place(degrees(-90)).wait_for_completed()
+	robot.drive_straight(distance_mm(150), speed_mmps(100)).wait_for_completed()
+	robot.turn_in_place(degrees(-90)).wait_for_completed()
+	robot.drive_straight(distance_mm(150), speed_mmps(100)).wait_for_completed()
+	# cozmo.world.World.request_nav_memory_map(robot, 1)
 	robot.set_lift_height(0.0).wait_for_completed()
 	robot.set_head_angle(degrees(0.0)).wait_for_completed()
-
+	
 	af = list()
-
+	
 	for i in range(num_cubes):
 
 		findcube, ok = fip(robot, af)
 		af.append(findcube)
 		
 		if ok == True:
-			robot.pickup_object(findcube, num_retries=3).wait_for_completed()
-			robot.go_to_object(charger, distance_mm(75)).wait_for_completed()
-			cozmo.robot.robot_alignment.RobotAlignmentTypes.Body = charger
-			robot.turn_in_place(degrees(90)).wait_for_completed()
-			robot.drive_straight(distance_mm(100), speed_mmps(100)).wait_for_completed()
-			robot.turn_in_place(degrees(-90)).wait_for_completed()
-			robot.drive_straight(distance_mm(stordist),speed_mmps(50)).wait_for_completed()
-			robot.set_lift_height(0).wait_for_completed()
-			robot.drive_straight(distance_mm(-stordist),speed_mmps(50)).wait_for_completed()
+			#robot.go_to_pose(findcube).wait_for_completed()
+			prev = copy.deepcopy(robot.pose)
+			x = robot.pickup_object(findcube, use_pre_dock_pose=False, in_parallel=False, num_retries=5).wait_for_completed()
+			#robot.go_to_object(findcube, distance_mm(30.0)).wait_for_completed()
+			#robot.drive_straight(distance_mm(10.0), speed_mmps(5)).wait_for_completed()
+			#robot.set_lift_height(1.00).wait_for_completed()
+			robot.go_to_pose(drop).wait_for_completed()
+			robot.drive_straight(distance_mm(stordist),speed_mmps(100)).wait_for_completed()
+			robot.set_lift_height(0.00).wait_for_completed()
+			robot.drive_straight(distance_mm(-stordist),speed_mmps(100)).wait_for_completed()
 			stordist -= storstep
+			robot.go_to_pose(prev).wait_for_completed()
+			#robot.go_to_object(charger, distance_mm(150.0)).wait_for_completed()
+			#cozmo.robot.robot_alignment.RobotAlignmentTypes.Body = charger
+			#robot.turn_in_place(degrees(90)).wait_for_completed()
+			#robot.drive_straight(distance_mm(100), speed_mmps(100)).wait_for_completed()
+			#robot.turn_in_place(degrees(-90)).wait_for_completed()
 
 cozmo.robot.Robot.drive_off_charger_on_connect = False
-cozmo.run_program(cube_return, use_viewer=True)
+cozmo.run_program(cube_return, use_viewer=True, use_3d_viewer=True)
