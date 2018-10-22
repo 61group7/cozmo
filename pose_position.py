4e3b271a32aca3dcb86fdd6e1d7f9d82faf11a05
unique username <43068204+beanfacts@users.noreply.github.com>
2018-10-22 18:15:24 +0700

    updated

--- pose_position.py
+++ pose_position.py
@@ -1,4 +1,4 @@
-import asyncio, functools, math, sys, cozmo, cv2, time, os
+import asyncio, functools, math, sys, cozmo, cv2, time, os, random
 import numpy as np
 import copy
 from multiprocessing import Process
@@ -7,3 +7,11 @@ from cozmo.util import degrees, distance_mm, radians, speed_mmps, Vector2
 from cozmo.lights import Color, Light
 from cozmo.objects import CustomObject, CustomObjectMarkers, CustomObjectTypes
 from PIL import Image, ImageColor, ImageDraw, ImageStat
+
+def main(robot):
+	print(robot.pose.position.x, robot.pose.position.y)
+	robot.drive_straight(random.randint(10,30))
+	robot.turn_in_place(degrees=random.randint(10,25))
+
+cozmo.robot.Robot.drive_off_charger_on_connect = False
+cozmo.run_program(main, use_viewer=True)
